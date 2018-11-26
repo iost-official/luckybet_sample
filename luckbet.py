db184202b46802448852a640edcaefa6ea029643
张卓 <mycinbrin@gmail.com>
2018-11-26 20:46:55 +0800

    fix

--- luckbet.py
+++ luckbet.py
@@ -5,15 +5,29 @@ import collections
 import time
 import re
 import json
+import argparse
+import random
+from multiprocessing.pool import ThreadPool
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--setup', dest='setup', action='store_true')
+parser.add_argument('--no-setup', dest='setup', action='store_false')
+parser.add_argument('--cid', dest='cid', type=str,
+                    help='contract id', default='')
+parser.set_defaults(setup=True)
 
 assert sys.version_info.major == 3
 assert sys.version_info.minor >= 6
 
 DEFAULT_EXPIRATION = 5
-DEFAULT_GASLIMIT = 100000  # (0.001 iost)
+DEFAULT_GASLIMIT = 5000000
+DEFAULT_GASPRICE = 100
 DEFAULT_NODEIP = '127.0.0.1'
 
-Account = collections.namedtuple('Account', ['nickname', 'account_name'])
+initial_coin_of_bet_user = 5
+
+Account = collections.namedtuple('Account', ['account_name'])
+command_prefix = f'iwallet --expiration {DEFAULT_EXPIRATION} --gaslimit {DEFAULT_GASLIMIT} --gasprice {DEFAULT_GASPRICE} '
 
 
 def log(s):
@@ -24,145 +38,132 @@ def check_float_equal(a, b):
     assert abs(a - b) < 1e-8
 
 
-def call(cmd):
+def call(cmd, verbose=False):
     log('exec ' + cmd)
-    if sys.platform == 'darwin':
-        # It is a workaround for a bug(feature?) of macOS.
-        IOST_REPO_PATH = '/Users/zhangzhuo/go-workspace/src/github.com/iost-official/go-iost'
-        DYLD_LIBRARY_PATH = os.environ.get(
-            'DYLD_LIBRARY_PATH') + f':{IOST_REPO_PATH}/vm/v8vm/v8/libv8/_darwin_amd64/'
-        cmd = 'DYLD_LIBRARY_PATH=' + DYLD_LIBRARY_PATH + ' ' + cmd
-    ret = subprocess.run(['bash', '-c', cmd], encoding='utf8',
-                         stdout=subprocess.PIPE, check=True)
+    ret = subprocess.run(cmd, encoding='utf8', shell=True,
+                         stdout=subprocess.PIPE)
     assert not ret.stdout is None
+    if verbose:
+        print(ret.stdout)
+    ret.check_returncode()
     return ret.stdout
 
 
-def create_account(nickname):
-    stdout = call('iwallet account -n ' + nickname)
-    account_name = re.findall('IOST\S+', stdout)[0]
-    return Account(nickname, account_name)
+def create_account(account_name, initial_ram, initial_gas_pledge, initial_balance):
+    call(f'{command_prefix} --account admin account --create {account_name} '
+         + f'--initial_ram {initial_ram} --initial_gas_pledge {initial_gas_pledge} --initial_balance {initial_balance}')
 
 
 def get_balance(account_name):
     cmd = 'iwallet balance ' + account_name
     stdout = call(cmd)
-    amount = re.findall('(\S+) iost', stdout)[0]
-    return float(stdout.split()[0])
-
-
-def check_tx(txid):
-    log(f'checking transaction {txid}')
-    cmd = f'curl -s -X GET http://{DEFAULT_NODEIP}:30001/getTxReceiptByTxHash/{txid}'
-    stdout = call(cmd)
-    print(f'output {stdout}')
-    result = json.loads(stdout)
-    assert result['txReceiptRaw']['succActionNum'] == 1
-    return result
+    amount = re.findall('"balance": (\S+),', stdout)[0]
+    return float(amount)
 
 
 def fetch_contract_state(cid, key):
-    cmd = f'curl -s -X GET http://{DEFAULT_NODEIP}:30001/getState/{cid}-{key}'
+    data = {"id": cid, "key": key}
+    cmd = f"curl -s -X POST --data '{json.dumps(data)}' http://{DEFAULT_NODEIP}:30001/getContractStorage"
     stdout = call(cmd)
-    json_result = eval(json.loads(stdout)['value'][1:])
+    json_result = eval(json.loads(stdout)['data'])
     return json_result
 
 
-def call_contract(cid, function_name, function_args, key=None):
-    cmd = f'iwallet call --expiration {DEFAULT_EXPIRATION}'
-    if not key is None:
-        cmd += f' -k {key}'
+def call_contract(caller_name, cid, function_name, function_args, verbose=False):
+    cmd = f'{command_prefix} --account {caller_name} call '
     function_args_str = json.dumps(function_args)
-    cmd += f' -l {DEFAULT_GASLIMIT} {cid} {function_name} \'{function_args_str}\''
+    cmd += f' {cid} {function_name} \'{function_args_str}\''
+    call(cmd, verbose)
+
+
+def publish_contract(js_file, js_abi_file, account_name):
+    cmd = f'{command_prefix} --account {account_name} compile {js_file} {js_abi_file}'
     stdout = call(cmd)
-    txid = re.findall(r'the transaction hash is: (\S+)', stdout)[0]
-    log(f'after call_contract, txid is {txid}')
-    time.sleep(DEFAULT_EXPIRATION)
-    return check_tx(txid)
-
-
-def transfer_from_initial(account_name, amount):
-    INITIAL_ACCOUNT = 'IOSTfQFocqDn7VrKV7vvPqhAQGyeFU9XMYo5SNn5yQbdbzC75wM7C'
-    INITIAL_KEY = '1rANSfcRzr4HkhbUFZ7L1Zp69JZZHiDDq5v7dNSbbEqeU4jxy3fszV4HGiaLQEyqVpS1dKT9g7zCVRxBVzuiUzB'
-    cid = 'iost.system'
-    function_name = 'Transfer'
-    function_args = [INITIAL_ACCOUNT, account_name, amount]
-    key = f'<(echo {INITIAL_KEY})'
-    call_contract(cid, function_name, function_args, key)
-    new_balance = get_balance(account_name)
-    assert new_balance >= amount
-    log(f'{account_name}: {new_balance} (after transfer {amount} iost in)')
-
-
-def publish_contract(js_file, js_abi_file, nickname):
-    cmd = f'iwallet compile -e {DEFAULT_EXPIRATION} -l {DEFAULT_GASLIMIT} -p 1 -k ~/.iwallet/{nickname}_ed25519 {js_file} {js_abi_file}'
-    stdout = call(cmd)
-    txid = re.findall(r'the transaction hash is: (\S+)', stdout)[0]
-    log(f'after publish_contract, txid is {txid}')
-    time.sleep(DEFAULT_EXPIRATION)
-    check_tx(txid)
-    contract_id = 'Contract' + txid
+    contract_id = re.findall(r'The contract id is (\S+)$', stdout)[0]
     return contract_id
 
 
-def main():
+def init_account():
+    private_key = '2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1'
+    cmd = f'iwallet account --import admin {private_key}'
+    call(cmd)
+
+
+def publish():
     # publish the contract
-    contract_uploader = create_account('uploader')
-    transfer_from_initial(contract_uploader.account_name, 100)
+    create_account('uploader', 50000, 10, 0)
     cid = publish_contract('contract/lucky_bet.js',
                            'contract/lucky_bet.js.abi', 'uploader')
+    return cid
 
-    bet_users = []
-    # create ten fake users
-    for i in range(1, 11):
-        nickname = f'user_{i}'
-        bet_users.append(create_account(nickname))
-    # give coins for the bet game to each user
-    initial_coins = 100
-    for account in bet_users:
-        transfer_from_initial(account.account_name, initial_coins)
+
+def get_bet_users():
+    bet_user_num = 10
+    bet_users = [
+        f'user_{random.randint(0, 1000000)}' for idx in range(bet_user_num)]
+
+    def create_bet_user(user):
+        create_account(user, 600, 10, initial_coin_of_bet_user)
+    pool = ThreadPool(bet_user_num)
+    pool.map(create_bet_user, bet_users)
+    return bet_users
+
+
+def main():
+    args = parser.parse_args()
+    if args.setup:
+        init_account()
+        cid = publish()
+    elif args.cid == '':
+        print("You must provide contract id if no setup is performed")
+        return
+    else:
+        cid = args.cid
+
+    # create ten fake users with IOSTs
+    bet_users = get_bet_users()
+    bet_user_num = len(bet_users)
+    pool = ThreadPool(bet_user_num)
+
     # bet
-    gas_used = []
-    bet_coins = []
-    for idx, account in enumerate(bet_users):
+    bet_coins = [idx // 2 + 1 for idx in range(bet_user_num)]
+
+    def bet(idx):
         lucky_number = idx
-        bet_coin = idx // 2 + 1
-        bet_coins.append(bet_coin)
-        nouce = idx + 1
-        args = [account.account_name, lucky_number,
-                bet_coin * 100000000, nouce]
-        result = call_contract(
-            cid, 'bet', args, key=f'~/.iwallet/{account.nickname}_ed25519')
-        gas_used.append(float(result['txReceiptRaw']['gasUsage']) / 1e8)
+        bet_coin = bet_coins[idx]
+        nouce = ''
+        args = [bet_users[idx], lucky_number, bet_coin, nouce]
+        call_contract(bet_users[idx], cid, 'bet', args, True)
+    pool.map(bet, range(bet_user_num))
+
     # get bet results
-    print('Balance after the bet')
-    final_balances = []
-    for account in bet_users:
-        balance = get_balance(account.account_name)
-        final_balances.append(balance)
-        print(f'{account.nickname}: {balance}')
+    final_balances = pool.map(get_balance, bet_users)
+    print('Balance after the bet', final_balances)
+
     # check result
-    contract_state = fetch_contract_state(cid, 'result1')
+    round_num = fetch_contract_state(cid, 'round')
+    contract_state = fetch_contract_state(cid, f'result{round_num-1}')
     rewards = []
     win_user_num = 0
     for record in contract_state['records']:
         if 'reward' in record:
             win_user_num += 1
-            rewards.append(float(record['reward']) / 1e8)
+            rewards.append(float(record['reward']))
         else:
             rewards.append(0)
     log(f'rewards: {rewards}')
     assert win_user_num == 1
     total_coins_bet = sum(bet_coins)
-    check_float_equal(total_coins_bet * 0.95, sum(rewards))
+    check_float_equal(total_coins_bet * 95 // 100, sum(rewards))
     # check balance of each user
     for i in range(10):
-        calculated_balance = initial_coins - \
-            bet_coins[i] - gas_used[i] + rewards[i]
+        calculated_balance = initial_coin_of_bet_user - \
+            bet_coins[i] + rewards[i]
         log(
             f'calculated_balance {calculated_balance} actual_balance {final_balances[i]}')
         check_float_equal(calculated_balance, final_balances[i])
     log('Congratulations! You have just run a smart contract on IOST!')
 
 
-main()
+if __name__ == '__main__':
+    main()
